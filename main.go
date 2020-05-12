ed836791f5fe9eb20df0b31b1b227ea0e404e0cd
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2020-05-12 23:20:16 +0200

    Draw black and white grid

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
-	"golang.org/x/image/colornames"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
+
+var (
+	cells      = 40
+	cellWidth  = 20
+	windowSize = float64(cells * cellWidth)
+)
+
+// type GameOfLife struct {
+// 	size int
+// }
+
+func draw(imd *imdraw.IMDraw) {
+	imd.Clear()
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			if (i%2 == 0 && j%2 == 1) || (i%2 == 1 && j%2 == 0) {
+				imd.Color = colornames.White
+			} else {
+				imd.Color = colornames.Black
+			}
+
+			imd.Push(pixel.V(float64(i*cellWidth), float64(j*cellWidth)))
+			imd.Push(pixel.V(float64(i*cellWidth+cellWidth), float64(j*cellWidth+cellWidth)))
+			imd.Rectangle(0)
+		}
+	}
+}
 
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title: "Game Of Life",
-		Bounds: pixel.R(0, 0, 800, 600),
-		VSync: true,
+		Title:  "Game Of Life",
+		Bounds: pixel.R(0, 0, windowSize, windowSize),
+		VSync:  true,
 	}
 
 	win, err := pixelgl.NewWindow(cfg)
@@ -19,15 +46,11 @@ func run() {
 		panic(err)
 	}
 
-	imd := imdraw.New(nil)
-	
-	imd.Color = colornames.Black
-	imd.Push(pixel.V(100, 100))
-	imd.Push(pixel.V(200, 200))
-	imd.Rectangle(0)
+	imd := *imdraw.New(nil)
 
 	for !win.Closed() {
 		win.Clear(colornames.White)
+		draw(&imd)
 		imd.Draw(win)
 		win.Update()
 	}
