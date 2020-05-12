4b1b4fd877dd1e4f1fb15e229ba1133326b1a16b
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2020-05-12 23:38:24 +0200

    Generate random grid

--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
+	"math/rand"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
-var (
-	cells      = 40
-	cellWidth  = 20
-	windowSize = float64(cells * cellWidth)
+const (
+	cells          = 40
+	cellWidth      = 20
+	windowSize     = float64(cells * cellWidth)
+	initalCellsPct = 0.3
 )
 
-// type GameOfLife struct {
-// 	size int
-// }
+type gameOfLife struct {
+	currentState [][]bool
+	size         int
+}
 
-func draw(imd *imdraw.IMDraw) {
-	imd.Clear()
+func (g *gameOfLife) initialize() {
+	g.currentState = make([][]bool, cells)
+	for i := range g.currentState {
+		g.currentState[i] = make([]bool, cells)
+	}
+
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			if (i%2 == 0 && j%2 == 1) || (i%2 == 1 && j%2 == 0) {
-				imd.Color = colornames.White
+			r := rand.Float32()
+			if r <= initalCellsPct {
+				g.currentState[i][j] = true
 			} else {
-				imd.Color = colornames.Black
+				g.currentState[i][j] = false
 			}
+		}
+	}
+}
 
-			imd.Push(pixel.V(float64(i*cellWidth), float64(j*cellWidth)))
-			imd.Push(pixel.V(float64(i*cellWidth+cellWidth), float64(j*cellWidth+cellWidth)))
-			imd.Rectangle(0)
+func (g *gameOfLife) draw(imd *imdraw.IMDraw) {
+	imd.Clear()
+	for i, row := range g.currentState {
+		for j, v := range row {
+			if v {
+				x0, y0 := float64(i*cellWidth), float64(j*cellWidth)
+				x1, y1 := float64(i*cellWidth+cellWidth), float64(j*cellWidth+cellWidth)
+
+				imd.Color = colornames.Black
+				imd.Push(pixel.V(x0, y0))
+				imd.Push(pixel.V(x1, y1))
+				imd.Rectangle(0)
+			}
 		}
 	}
 }
@@ -46,11 +68,15 @@ func run() {
 		panic(err)
 	}
 
+	game := gameOfLife{}
+	// game.currentState = make([][]bool, cells)
+	game.initialize()
+
 	imd := *imdraw.New(nil)
 
 	for !win.Closed() {
 		win.Clear(colornames.White)
-		draw(&imd)
+		game.draw(&imd)
 		imd.Draw(win)
 		win.Update()
 	}
